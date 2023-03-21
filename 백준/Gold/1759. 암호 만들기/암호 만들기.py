0c904e92f0ab573cc453141d77662a5653909b79
giujae <122589766+giujae@users.noreply.github.com>
2023-03-21 17:30:11 +0900

    [Gold V] Title: 암호 만들기, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,14 +1,20 @@
+import sys
 from itertools import combinations
- 
-L, C = map(int,input().split())
-alpha = sorted(input().split())
-words = combinations(alpha, L)
- 
-for word in words:
-    cnt_vow = 0
-    for i in word:
-        if i in "aeiou":
-            cnt_vow += 1
- 
-    if cnt_vow >=1 and L - cnt_vow >=2:
-        print(''.join(word))
+
+L, C = map(int, sys.stdin.readline().split())
+char = list(map(str, sys.stdin.readline().split()))
+answer = []
+lst = []
+possible = list(combinations(char, L))
+for i in possible:
+    answer.append(''.join(sorted(i)))
+for i in answer:
+    vow = 0
+    for j in i:
+        if j in ['a', 'e', 'i', 'o', 'u']:
+            vow += 1
+    if L-vow >= 2 and vow >= 1:
+        lst.append(i)
+lst.sort()
+for i in lst:
+    print(i)
