bbf185ccb67fdd04ce8dc4d105644dce0d1864f6
giujae <122589766+giujae@users.noreply.github.com>
2023-03-24 15:44:38 +0900

    [Gold IV] Title: 전화번호 목록, Time: 224 ms, Memory: 117720 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -1,22 +1,16 @@
 import sys
 input = sys.stdin.readline
 
-t = int(input())
-for _ in range(t):
+T = int(input())
+for tc in range(1, T+1):
     n = int(input())
-    call = [str(input().strip()) for _ in range(n)] # 전화번호를 문자열로 받는다.
-    call.sort() # 오름차순으로 정렬하여 사전순으로 정렬
-    chek = "yes" # 일관성이 있는지 체크
-
-    # 반복문을 통해 전화번호를 확인
-    for i in range(len(call) - 1):
-
-        # 현재 전화번호의 문자열과 다음 전화번호의 현재 전화번호 길이만큼의 문자열과 같은지 확인
-        # 같으면 일관성이 없는 것
-        if call[i] == call[i + 1][0:len(call[i])]:
-            chek = "no"
-
-    if chek == "no":
-        print("NO")
+    number = [str(input().strip()) for _ in range(n)]
+    number.sort()
+    temp = True
+    for i in range(len(number)-1):
+        if number[i] == number[i+1][:len(number[i])]:
+            temp = False
+    if temp:
+        print('YES')
     else:
-        print("YES")
+        print('NO')
